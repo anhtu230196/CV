273ce8716935675d27bfa80a1cd51d05663116a8
Anh Tu <anhtu23011996@gmail.com>
2022-11-10 19:37:20 +0700

    navigate fix

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ const noButton = document.querySelector('.no-button')
 const heartContainer = document.querySelector('.heart-content')
 
 yesButton.addEventListener('click', function(){
-  modal.style.visibility = 'hidden'
-  heartContainer.classList.add('active')
+  window.location.href = '/heart.html'
 })
 
 noButton.addEventListener('mouseover', function(){
+  const randomHeight = Math.floor(Math.random() * 40) + 30
+  const randomWidth = Math.floor(Math.random() * 40) + 30
+  console.log(randomHeight)
+  modal.style.top = `${randomHeight}%`
+  modal.style.left = `${randomWidth}%`
+})
+
+noButton.addEventListener('click', function() {
   const randomHeight = Math.floor(Math.random() * 40) + 30
   const randomWidth = Math.floor(Math.random() * 40) + 30
   console.log(randomHeight)
